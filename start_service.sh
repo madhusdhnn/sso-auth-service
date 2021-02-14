46a96192cac64658a773af2fee20752391fe3620
madhusdhnn <madhu.padhamanaban298@gmail.com>
2021-02-15 01:14:16 +0530

    Add bash prefix to call the file

--- start_service.sh
+++ start_service.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-./seed.sh
+bash ./seed.sh
 ./gradlew clean build -x test
 java -Dspring.config.location=config/development/application.yml -jar ./build/libs/auth-service.jar
